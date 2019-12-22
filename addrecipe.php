09eeaecd34acc1c5afa79f9e6dc32ff3c1cc5116
duhowise <duhwise@gmail.com>
2019-12-22 17:46:17 +0000

    Changed themes

--- addrecipe.php
+++ addrecipe.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/3/united/bootstrap.min.css">
     <link rel="stylesheet" href="main.css">
     <title>Add your recipe</title>
 </head>
